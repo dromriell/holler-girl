a0c6c768c40224a71584fcc5c685f1370adc9d46
dromriell <devonromriell@gmail.com>
2022-04-15 22:44:35 -0400

    - Add additional pages and routes - Remove console log

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import HeaderBar from "./components/HeaderBar";
 import SideMenu from "./components/SideMenu";
 
-import Home from "./components/home/Home";
-import Tickets from "./components/tickets/Tickets";
+import HomePage from "./components/home/HomePage";
+import TicketsPage from "./components/tickets/TicketsPage";
+import LineupPage from "./components/lineup/LineupPage";
+import CampingPage from "./components/camping/CampingPage";
+import MerchPage from "./components/merch/MerchPage";
 
 function App() {
   const location = useLocation();
@@ -14,7 +17,6 @@ function App() {
   const handleMenuToggle = () => {
     setIsMenuOpen(!isMenuOpen);
   };
-  console.log(location);
   return (
     <main>
       <HeaderBar handleMenuToggle={handleMenuToggle} />
@@ -24,16 +26,14 @@ function App() {
         currentPath={location.pathname}
       />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tickets" element={<Tickets />} />
-        <Route path="/users" element={<Users />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/tickets" element={<TicketsPage />} />
+        <Route path="/lineup" element={<LineupPage />} />
+        <Route path="/camping" element={<CampingPage />} />
+        <Route path="/merch" element={<MerchPage />} />
       </Routes>
     </main>
   );
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
 
 export default App;
