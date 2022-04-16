e00ce296e0c86589d0a476a58a2e0ea7e32395bc
dromriell <devonromriell@gmail.com>
2022-04-15 23:14:19 -0400

    - Add event data

--- src/common/siteData.js
+++ src/common/siteData.js
@@ -31,3 +31,21 @@ export const ticketData = [
     details: ["All ages", "Per person"],
   },
 ];
+
+export const eventData = [
+  {
+    name: "camping",
+    url: "https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    description: "Camping camping camping",
+  },
+  {
+    name: "yoga",
+    url: "https://images.pexels.com/photos/4793296/pexels-photo-4793296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    description: "Yoga yoga yoga",
+  },
+  {
+    name: "movie",
+    url: "https://images.pexels.com/photos/33129/popcorn-movie-party-entertainment.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    description: "Movie movie movie",
+  },
+];
