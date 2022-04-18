c151163e1d07d17758d08a89884a0d92d0e2b052
dromriell <devonromriell@gmail.com>
2022-04-18 14:14:44 -0400

    - Add Clouds component to border top

--- src/components/home/LineupSection.js
+++ src/components/home/LineupSection.js
@@ -1,13 +1,23 @@
+import Clouds from "./Clouds";
+
 const LineupSection = (props) => {
-  const { lineupData } = props;
+  const { lineupData, pageOffset } = props;
   return (
-    <section id="lineup">
-      <ul>
-        {lineupData.map((artist, idx) => {
-          return <li key={`artist-${idx}`}>{artist}</li>;
-        })}
-      </ul>
-    </section>
+    <article id="lineupContainer">
+      <Clouds pageOffset={pageOffset} />
+      <div id="lineup">
+        <h2>Line-up</h2>
+        <ul>
+          {lineupData.map((artist, idx) => {
+            return (
+              <li key={`artist-${idx}`}>
+                <h3>{artist}</h3>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </article>
   );
 };
 
