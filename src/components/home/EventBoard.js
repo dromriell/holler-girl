b060b66d1268ab4ae82a8ce45e320fa6daa8ae46
dromriell <devonromriell@gmail.com>
2022-04-18 14:13:13 -0400

    - Refactor EventBoard to use Event component

--- src/components/home/EventBoard.js
+++ src/components/home/EventBoard.js
@@ -1,17 +1,15 @@
-// Event Board Imports
-import { GrYoga } from "react-icons/gr";
-import { FaCampground } from "react-icons/fa";
-import { RiMovie2Line } from "react-icons/ri";
+import { eventData } from "../../common/siteData";
 
 const Event = (props) => {
-  const { background, Icon } = props;
+  const { name, url, description } = props;
 
   return (
     <div className="event">
-      <div className="eventIcon" style={{ backgroundColor: background }}>
-        {Icon}
-      </div>
-      <h5>{props.children}</h5>
+      <div
+        className="eventBackground"
+        style={{ backgroundImage: `url('${url}')` }}
+      ></div>
+      <h5>{description}</h5>
     </div>
   );
 };
@@ -19,15 +17,9 @@ const Event = (props) => {
 const EventBoard = (props) => {
   return (
     <section id="eventBoard">
-      <Event background="#fcfcf9" Icon={<FaCampground />}>
-        On-site camping
-      </Event>
-      <Event background="#a1869e" Icon={<GrYoga />}>
-        Sat. Morning Yoga w/ Ashley Shochat
-      </Event>
-      <Event background="#fdf7a3" Icon={<RiMovie2Line />}>
-        Friday Night Movie
-      </Event>
+      {eventData.map((event, idx) => {
+        return <Event {...event} key={idx} />;
+      })}
     </section>
   );
 };
