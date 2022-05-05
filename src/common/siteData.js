578a45250de556f559bd079744f43f78a53d49e2
dromriell <devonromriell@gmail.com>
2022-05-04 23:42:25 -0400

    - Update lineup - Change eventData layout

--- src/common/siteData.js
+++ src/common/siteData.js
@@ -1,22 +1,33 @@
+import camping01 from "../assets/camping01.webp";
+import camping02 from "../assets/camping02.webp";
+import camping03 from "../assets/camping03.webp";
+
+import gh17 from "../assets/greenhouse17.webp";
+
 export const lineupData = [
-  "Brittany Dawn Samples",
-  "Em Jamison",
-  "Madylin Goins",
   "Rachel Crowe",
   "Ande Fee",
-  "Kadie Meadows",
-  "Emma Le",
-  "Melody Youngblood",
   "Honey Wright",
-  "Erin Sliney",
-  "Tiffany Williams",
+  "Emma Lee",
+  "Sarah Fightmaster",
   "Mary Wright",
   "Lemon Grass",
-  "Sarah Fightmaster",
-  "Jen Marie",
   "Anna Kline",
+  "Second Rodeo",
+  "Brittany Dawn Samples",
+  "Erin Sliney",
+  "Jenn Marie",
+  "Elizabeth Hazlett",
   "Liz Frazier",
+  "Madylin Goins",
+  "Huck & Jesse",
+  "Em Jamison",
+  "Kadie Meadows",
+  "Melody Youngblood",
   "Sydney Adams",
+  "Elizabeth Walker",
+  "Teresa Prince",
+  "Slut Pill",
 ];
 
 export const ticketData = [
@@ -32,20 +43,26 @@ export const ticketData = [
   },
 ];
 
-export const eventData = [
-  {
-    name: "camping",
-    url: "https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    description: "Camping",
-  },
-  {
-    name: "yoga",
-    url: "https://images.pexels.com/photos/4793296/pexels-photo-4793296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    description: "Yoga w/ Ashley Shochat",
-  },
-  {
-    name: "movie",
-    url: "https://images.pexels.com/photos/33129/popcorn-movie-party-entertainment.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    description: "Friday Night Movie",
-  },
-];
+export const eventData = {
+  camping: [
+    {
+      name: "camping",
+      images: [camping01, camping02, camping03],
+      description: "RV, Tent, & Car Camping",
+    },
+  ],
+  activities: [
+    {
+      name: "yoga",
+      images: [
+        "https://images.pexels.com/photos/4793296/pexels-photo-4793296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      ],
+      description: "Saturday Morning Yoga w/ Ashley Shochat",
+    },
+    {
+      name: "auction",
+      images: [gh17],
+      description: "Silent Art Auction in Support of GreenHouse17",
+    },
+  ],
+};
