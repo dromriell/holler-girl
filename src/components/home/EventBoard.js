20d347abc16a16d2f9eebcd2b4b831b416b69f7a
dromriell <devonromriell@gmail.com>
2022-05-04 23:43:18 -0400

    - Seperate event sections into seperate components

--- src/components/home/EventBoard.js
+++ src/components/home/EventBoard.js
@@ -1,25 +1,62 @@
 import { eventData } from "../../common/siteData";
 
 const Event = (props) => {
-  const { name, url, description } = props;
+  const { isBorder, images, description } = props;
 
   return (
     <div className="event">
-      <div
-        className="eventBackground"
-        style={{ backgroundImage: `url('${url}')` }}
-      ></div>
       <h5>{description}</h5>
+      {images.map((image) => {
+        return (
+          <div
+            className="eventBackground"
+            style={{ backgroundImage: `url('${image}')` }}
+          ></div>
+        );
+      })}
+    </div>
+  );
+};
+
+const CampingSection = (props) => {
+  return (
+    <div id="campingSection" className="eventSection">
+      {eventData.camping.map((event, idx) => {
+        return <Event {...event} key={idx} />;
+      })}
+    </div>
+  );
+};
+
+const ActivitiesSection = (props) => {
+  return (
+    <div id="activitiesSection" className="eventSection">
+      <div class="custom-shape-divider-top-1651719041">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+            class="shape-fill"
+          ></path>
+        </svg>
+      </div>
+      {eventData.activities.map((event, idx) => {
+        return <Event {...event} key={idx} />;
+      })}
     </div>
   );
 };
 
 const EventBoard = (props) => {
+  const { borderEvent } = props;
   return (
     <section id="eventBoard">
-      {eventData.map((event, idx) => {
-        return <Event {...event} key={idx} />;
-      })}
+      <CampingSection />
+      <ActivitiesSection />
     </section>
   );
 };
