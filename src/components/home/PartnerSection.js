4f2bcfa6b673575c878571408e5e45034dc05398
dromriell <devonromriell@gmail.com>
2022-05-06 23:48:58 -0400

    - Create Partner component - Create section layout

--- src/components/home/PartnerSection.js
+++ src/components/home/PartnerSection.js
@@ -1,9 +1,25 @@
+import partnersTitle from "../../assets/partnersText.svg";
+import { partnersArray } from "../../common/siteData";
+
+const Partner = (props) => {
+  const { name, logoSrc } = props;
+  return (
+    <li className="partnerContainer">
+      <img src={logoSrc} alt={`${name}-logo`} />
+    </li>
+  );
+};
+
 const PartnerSection = (props) => {
   return (
-    <article id="partnerSection">
-      <img src="" alt="Partners" />
-      <div></div>
-    </article>
+    <section id="partnersSection">
+      <img src={partnersTitle} alt="Partners" />
+      <ul className="partnersList">
+        {partnersArray.map((partnerInfo) => {
+          return <Partner {...partnerInfo} />;
+        })}
+      </ul>
+    </section>
   );
 };
 
