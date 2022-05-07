35c9f732f76d60dd48d088189036e149800b473c
dromriell <devonromriell@gmail.com>
2022-05-06 22:12:16 -0400

    - First commit

---
+++ src/components/home/PartnerSection.js
@@ -0,0 +1,10 @@
+const PartnerSection = (props) => {
+  return (
+    <article id="partnerSection">
+      <img src="" alt="Partners" />
+      <div></div>
+    </article>
+  );
+};
+
+export default PartnerSection;
