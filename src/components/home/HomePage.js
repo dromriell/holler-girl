0b065d7f1625fd980bb343703e94fcf20a8c3e9a
dromriell <devonromriell@gmail.com>
2022-05-07 21:45:17 -0400

    - Comment out PartnerSection

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -19,7 +19,7 @@ const HomePage = () => {
       {!isLoaded && <LoadingOverlay />}
       <HomeHero setIsLoaded={setIsLoaded} />
       <EventBoard />
-      <PartnerSection />
+      {/* <PartnerSection /> */}
       <section id="map">
         <div
           className="mapBackground"
