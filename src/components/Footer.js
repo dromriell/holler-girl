4b25fc3a334f459cda2c0485ab5888064fbb1efb
dromriell <devonromriell@gmail.com>
2022-05-08 17:30:29 -0400

    - Add aria-label to links - Add footer copyright and message - Update logo to new logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import hollerGirlLogo from "../assets/HollerGirl.webp";
+import logoText from "../assets/logoText.svg";
 import { ReactComponent as FacebookLogo } from "../assets/facebook.svg";
 import { ReactComponent as TwitterLogo } from "../assets/twitter.svg";
 import { ReactComponent as EmailLogo } from "../assets/email.svg";
@@ -9,25 +9,32 @@ const Footer = (props) => {
   return (
     <footer id="footer">
       <div>
-        <img src={hollerGirlLogo} alt={"hollerGirlLogo"} />
+        <img src={logoText} alt={"hollerGirlLogo"} />
       </div>
       <div className="linkRow">
-        <a href="https://www.facebook.com/HollerGirlmusic">
+        <a
+          href="https://www.facebook.com/HollerGirlmusic"
+          aria-label="Facebook"
+        >
           <FacebookLogo />
         </a>
-        <a href="#">
+        <a href="#" aria-label="Twitter">
           <TwitterLogo />
         </a>
-        <a href="#">
+        <a href="#" aria-label="Email">
           <EmailLogo />
         </a>
-        <a href="#">
+        <a href="#" aria-label="Youtube">
           <YoutubeLogo />
         </a>
-        <a href="#">
+        <a href="#" aria-label="Instagram">
           <InstagramLogo />
         </a>
       </div>
+      <p>
+        © HollerGirl Music Festival 2022 - Logo By Sarah Fightmaster - Built By
+        Devon Romriell
+      </p>
     </footer>
   );
 };
