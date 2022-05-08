456c299801390f780c0d3a879562fd5129539292
dromriell <devonromriell@gmail.com>
2022-05-08 17:31:55 -0400

    - Change Event structure - Add key to each Event image

--- src/components/home/EventBoard.js
+++ src/components/home/EventBoard.js
@@ -2,19 +2,23 @@ import { eventData } from "../../common/siteData";
 import siteTitle from "../../assets/siteText.svg";
 
 const Event = (props) => {
-  const { images, name, description } = props;
+  const { name, images, title, description } = props;
 
   return (
     <div className="event">
-      <p>{description}</p>
-      {images.map((image) => {
-        return (
-          <div
-            className="eventBackground"
-            style={{ backgroundImage: `url('${image}')` }}
-          ></div>
-        );
-      })}
+      <h3>{title}</h3>
+      <div className="eventContent">
+        {images.map((image, idx) => {
+          return (
+            <div
+              className="eventBackground"
+              style={{ backgroundImage: `url('${image}')` }}
+              key={`${name}${idx}`}
+            ></div>
+          );
+        })}
+        {description ? <p>{description}</p> : null}
+      </div>
     </div>
   );
 };
@@ -25,7 +29,7 @@ const CampingSection = (props) => {
       <img src={siteTitle} alt="The Site" />
       {siteTitle}
       {eventData.camping.map((event, idx) => {
-        return <Event {...event} key={idx} />;
+        return <Event {...event} key={`campingEvent${idx}`} />;
       })}
       <button className="ctaButton">
         <a href="/info/">
@@ -39,7 +43,7 @@ const CampingSection = (props) => {
 const ActivitiesSection = (props) => {
   return (
     <div id="activitiesSection" className="eventSection">
-      <div class="custom-shape-divider-top-1651719041">
+      <div className="custom-shape-divider-top-1651719041">
         <svg
           data-name="Layer 1"
           xmlns="http://www.w3.org/2000/svg"
@@ -48,14 +52,14 @@ const ActivitiesSection = (props) => {
         >
           <path
             d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-            class="shape-fill"
+            className="shape-fill"
           ></path>
         </svg>
       </div>
       {eventData.activities.map((event, idx) => {
-        return <Event {...event} key={idx} />;
+        return <Event {...event} key={`activitiesEvent${idx}`} />;
       })}
-      <div class="custom-shape-divider-bottom-1651893184">
+      <div className="custom-shape-divider-bottom-1651893184">
         <svg
           data-name="Layer 1"
           xmlns="http://www.w3.org/2000/svg"
@@ -64,7 +68,7 @@ const ActivitiesSection = (props) => {
         >
           <path
             d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-            class="shape-fill"
+            className="shape-fill"
           ></path>
         </svg>
       </div>
