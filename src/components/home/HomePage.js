c010137383c37d74790ab4987fa06b9c2eb59042
dromriell <devonromriell@gmail.com>
2022-05-10 11:04:37 -0400

    - dangerouslySetInnerHTML script w/ event schema

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -5,22 +5,30 @@ import EventBoard from "./EventBoard";
 import Footer from "../Footer";
 import LoadingOverlay from "../LoadingOverlay";
 import MapSection from "./MapSection";
+import { eventSchema } from "../../common/siteData";
 
 const HomePage = () => {
   const [isLoaded, setIsLoaded] = useState(false);
-
   return (
-    <article
-      id="homePage"
-      className="noScroll"
-      style={!isLoaded ? { overflow: "hidden", maxHeight: "100vh" } : undefined}
-    >
-      {!isLoaded && <LoadingOverlay />}
-      <HeroSection setIsLoaded={setIsLoaded} />
-      <EventBoard />
-      <MapSection />
-      <Footer />
-    </article>
+    <>
+      <article
+        id="homePage"
+        className="noScroll"
+        style={
+          !isLoaded ? { overflow: "hidden", maxHeight: "100vh" } : undefined
+        }
+      >
+        {!isLoaded && <LoadingOverlay />}
+        <HeroSection setIsLoaded={setIsLoaded} />
+        <EventBoard />
+        <MapSection />
+        <Footer />
+      </article>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(eventSchema) }}
+      ></script>
+    </>
   );
 };
 
